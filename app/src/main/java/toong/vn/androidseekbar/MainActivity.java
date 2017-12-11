26afc20e43f4327e0a8fdf890b3a842d2a6c2912
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2017-12-11 13:29:48 +0700

    first commit

---
+++ app/src/main/java/toong/vn/androidseekbar/MainActivity.java
@@ -0,0 +1,35 @@
+package toong.vn.androidseekbar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    AppCompatSeekBar seekBar;
+    TextView tvProgress;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekBar);
+        tvProgress = findViewById(R.id.text_progress);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvProgress.setText(""+progress);
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+}
